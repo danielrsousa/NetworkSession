a9d258032d2001b47940ab47721e163a8bcc95cc
Daniel Rocha <danrocha@uolinc.com>
2020-12-02 12:31:19 -0300

    Adicionar  MyViewClass

---
+++ NetworkSession/MyViewClass.swift
@@ -0,0 +1,22 @@
+//
+//  MyViewClass.swift
+//  NetworkSession
+//
+//  Created by Daniel Rocha de Sousa on 02/12/20.
+//  Copyright © 2020 Daniel Rocha. All rights reserved.
+//
+
+import UIKit
+
+@objc
+public class MyViewClass: UIView {
+    
+    @objc
+    init(teste: String) {
+        super.init(frame: CGRect.zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
